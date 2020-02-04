ef295a133216c5f8f406ec39bc36edec9074b06f
noirvin <vin.sp97@googlemail.com>
2020-02-03 22:26:37 -0800

    crud done, ready for deployment

--- app.py
+++ app.py
@@ -40,26 +40,30 @@ def guitars_show(guitar_id):
     guitar_reviews = reviews.find({'guitar_id': ObjectId(guitar_id)})
     return render_template('guitars_show.html', guitar=guitar, reviews=guitar_reviews)
 
-@app.route('/guitars/<guitar_id>/reviews/<review_id>/edit')
+@app.route('/reviews/<review_id>/edit')
 def reviews_edit(review_id):
-    """Show the edit form for a review."""
+    """Show the edit form for a product."""
     review = reviews.find_one({'_id': ObjectId(review_id)})
-    return render_template('reviews_edit.html', review=review, title='Edit Review')
+    guitar = guitars.find_one({'_id': ObjectId(review['guitar_id'])})
+    return render_template('reviews_edit.html', review=review, guitar=guitar)
 
-@app.route('/guitars/reviews/<review_id>', methods=['POST'])
-def reviews_save(review_id):
-    """Save an edited review."""
-    saved_review = {
-        'title': request.form.get('title'),
+@app.route('/reviews/<review_id>', methods =['POST'])
+def update_review(review_id):
+    """Show the edit form for a review."""
+    updated_review = {
+        'subject': request.form.get('title'),
         'content': request.form.get('content'),
-    }
+        'guitar_id': ObjectId(request.form.get('guitar_id'))
+        }
     reviews.update_one(
         {'_id': ObjectId(review_id)},
-        {'$set': saved_review})
-    return redirect(url_for('guitars_show', review_id=review_id))
+        {'$set': updated_review})
+    guitar = guitars.find_one({'_id': ObjectId(updated_review['guitar_id'])})
+    g_id = guitar['_id']
+    return redirect(url_for('guitars_show', guitar_id = g_id))
 
 
-@app.route('/guitars/reviews/new', methods=['POST'])
+@app.route('/guitars/reviews', methods=['POST'])
 def reviews_new():
     """Submit a new review."""
     review = {
@@ -69,19 +73,16 @@ def reviews_new():
     }
 
     g_id=request.form.get('guitar_id')
-    guitar = guitars.find_one({'_id': ObjectId(g_id)})
 
     review_id = reviews.insert_one(review).inserted_id
-    rs = reviews.find({'guitar_id': ObjectId(g_id)})
-    #return render_template('guitars_show.html', guitar_id=guitar_id, guitar=guitar, reviews=rs)
     return redirect(url_for('guitars_show', guitar_id=g_id))
 
-@app.route('/guitars/guitars/<guitar_id>/reviews/<review_id>/delete', methods=['POST'])
+@app.route('/reviews/<review_id>/delete', methods=['POST'])
 def reviews_delete(review_id):
     """delete a review."""
-    review = reviews.find_one({'_id': ObjectId(review_id)})
+    guitar_id = request.form.get('guitar_id')
     reviews.delete_one({'_id': ObjectId(review_id)})
-    return redirect(url_for('guitars_show', guitar_id=guitar.get('guitar_id')))
+    return redirect(url_for('guitars_show',guitar_id = guitar_id))
 
 
 
